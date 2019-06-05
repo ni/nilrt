d8d03f8789596bb079172c17636cfca54043a683
Haris Okanovic <haris.okanovic@ni.com>
2019-06-05 17:03:04 -0500

    scripts/buildVM: Change machine definition files to use EFI
    scripts/buildVM-files/machine-def-x64.vbox
        Add `<Firmware type="EFI"/>` line.
    
    scripts/buildVM-files/machine-def-x64.vmx
        Add `firmware = "efi"` line.
    
    Signed-off-by: Haris Okanovic <haris.okanovic@ni.com>
    Natinst-ReviewBoard-ID: 294951

--- scripts/buildVM-files/runQemuVM.sh
+++ scripts/buildVM-files/runQemuVM.sh
@@ -5,7 +5,7 @@ qemu-system-x86_64 \
     -enable-kvm -cpu kvm64 -smp cpus=1 \
     -m "${VM_MEM_SIZE_MB}" \
     -nographic \
-    -drive if=pflash,format=raw,readonly,file=$SCRIPT_DIR/OVMF/OVMF_CODE.fd \
-    -drive if=pflash,format=raw,file=$SCRIPT_DIR/OVMF/OVMF_VARS.fd \
+    -drive if=pflash,format=raw,readonly,file="$SCRIPT_DIR/OVMF/OVMF_CODE.fd" \
+    -drive if=pflash,format=raw,file="$SCRIPT_DIR/OVMF/OVMF_VARS.fd" \
     -drive file="$SCRIPT_DIR/${VM_NAME}.qcow2",index=0,media=disk
 echo "DONE"
