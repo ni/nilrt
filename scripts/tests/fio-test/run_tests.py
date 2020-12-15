c775214f08a29ec8bbf3ef5911d7cc0af8eb375e
Chaitanya Vadrevu <chaitanya.vadrevu@ni.com>
2020-12-15 14:33:46 -0600

    fio-test: Update ioengine from libaio to sync
    Update ioengine to sync and minor improvements to
    run_tests.py
    
    Signed-off-by: Chaitanya Vadrevu <chaitanya.vadrevu@ni.com>

--- scripts/tests/fio-test/run_tests.py
+++ scripts/tests/fio-test/run_tests.py
@@ -1,8 +1,13 @@
 #!/bin/bash
-opkg update
-opkg install fio
+which fio &> /dev/null || (opkg update; opkg install fio; sleep 5;)
+
+hn=`hostname`
+un=`uname -r`
+dt=`date "+%F-%T"`
+fn="$hn"__"$un"__"$dt".output
 
 for i in *.fio; do
-    echo =========$i========= &>> fio.output;
-    fio $i &>> fio.output;
+    echo =========$i========= &>> $fn;
+    fio $i &>> $fn;
+    sleep 5;
 done
