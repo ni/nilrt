5b6aa3a5bf169b8a3ea4cc1af28bdb06d1d02484
Shruthi Ravichandran <shruthi.ravichandran@ni.com>
2021-03-30 15:14:12 -0500

    Miscellaneous updates and bug fixes.
    - Add `set -e`
    - Replace `fetch` with `pull`
    - Use quotes when referring to the argo cache and local sstate cache
      dirs
    - Add a trap to delete the .latest directory if things go wrong
    - Create a DONOTUSE file in the remote cache directory location
      before copying the cache in a push operation. This is to safeguard
      against pulling the cache directory before the mv operation can be
      completed, which could result in an incomplete local cache.
      The pull operation will now only fetch the latest directory from
      argo that does not have the .latest suffix as well as the DONOTUSE
      file.
    - Sort and remove older cache directories on argo based on the name
      of the directories instead of file modification time.
    
    Signed-off-by: Shruthi Ravichandran <shruthi.ravichandran@ni.com>

--- scripts/azdo/update_sstate_cache.sh
+++ scripts/azdo/update_sstate_cache.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 usage() {
 	local exit_code=${1:-2}
@@ -11,7 +12,7 @@ $(basename $BASH_SOURCE) push local_sstate_cache_dir argo_sstate_cache_dir
    Push the local sstate-cache directory contents to the remote argo
    server directory.
 
-$(basename $BASH_SOURCE) fetch local_sstate_cache_dir argo_sstate_cache_dir
+$(basename $BASH_SOURCE) pull local_sstate_cache_dir argo_sstate_cache_dir
    Fetch the latest sstate cache from the remote argo cache directory
    and install to the local sstate-cache directory.
 
@@ -27,21 +28,29 @@ EOF
 }
 
 push_cache() {
-	if [ ! -d $local_sstate_cache_dir -o -z "$(ls -A $local_sstate_cache_dir 2>/dev/null)" ]; then
+	if [ ! -d "$local_sstate_cache_dir" -o -z "$(ls -A $local_sstate_cache_dir 2>/dev/null)" ]; then
 		echo $local_sstate_cache_dir is missing/empty. Please provide a valid local_sstate_cache_dir name with sstate-cache contents.
 		usage
 	fi
 
-	mkdir -p $argo_sstate_cache_dir
-	pushd $argo_sstate_cache_dir >/dev/null
+	mkdir -p "$argo_sstate_cache_dir"
+	pushd "$argo_sstate_cache_dir" >/dev/null
 
 	# Copy the contents of the cache to a temp .latest directory
-	# before removing the .latest suffix from the name.
+	# before removing the .latest suffix from the name. Create a
+	# DONOTUSE file in the .latest directory until the mv operation is
+	# complete. The pull operation will not rsync a folder with this
+	# file. Implement this extra precaution because the mv may not be
+	# atomic on the argo share.
+
 	temp_argo_dir=$(date +"%s").latest
+	trap "{ rm -rf $temp_argo_dir; }" EXIT
 	mkdir $temp_argo_dir
+	touch $temp_argo_dir/DONOTUSE
 	echo Pushing sstate cache to argo at $temp_argo_dir
-	cp -rL $local_sstate_cache_dir/* $temp_argo_dir/
+	cp -rL "$local_sstate_cache_dir"/* $temp_argo_dir/
 	mv $temp_argo_dir ${temp_argo_dir%.latest}
+	rm ${temp_argo_dir%.latest}/DONOTUSE
 
 	# Cap the number of cache directories on the argo server at
 	# max_dir_count. Delete older cache directories exceeding the
@@ -49,7 +58,7 @@ push_cache() {
 	max_dir_count=5  # arbitrarily chosen number
 	current_dir_count=$(find * -maxdepth 0 -type d 2> /dev/null | wc -l)
 	if [ $current_dir_count -gt $max_dir_count ]; then
-		dirs=$(ls -trd * | head -$(($current_dir_count - $max_dir_count)))
+		dirs=$(ls -d * | head -$(($current_dir_count - $max_dir_count)))
 		for dir in ${dirs[@]}; do
 			echo Removing old sstate cache directory $dir
 			rm -rf $dir
@@ -59,20 +68,25 @@ push_cache() {
 	popd >/dev/null
 }
 
-fetch_cache() {
-	if [ ! -d $argo_sstate_cache_dir ]; then
+pull_cache() {
+	if [ ! -d "$argo_sstate_cache_dir" ]; then
 		echo Cannot find argo sstate cache directory $argo_sstate_cache_dir
 		usage
 	fi
 
-	pushd $argo_sstate_cache_dir >/dev/null
+	mkdir -p "$local_sstate_cache_dir"
+	pushd "$argo_sstate_cache_dir" >/dev/null
 
-	mkdir -p $local_sstate_cache_dir
 	# Rsync the latest cache directory, excluding any directory
 	# with a .latest suffix from the search.
-	latest_cache_dir=$(ls -t -I *.latest | head -1)
+	latest_cache_dir=$(ls -r -I *.latest | sed -n 1p)
+	# Exclude a directory with the DONOTUSE file.
+	if [ -e $latest_cache_dir/DONOTUSE ]; then
+		latest_cache_dir=$(ls -r -I *.latest | sed -n 2p)
+	fi
+
 	echo Copying over the cache contents from $latest_cache_dir. Might take a while...
-	rsync -rltxSWh --info=STATS1 $latest_cache_dir/ $local_sstate_cache_dir/
+	rsync -rltxSWh --info=STATS1 "$latest_cache_dir"/ "$local_sstate_cache_dir"/
 
 	popd >/dev/null
 }
@@ -95,9 +109,9 @@ case "$1" in
 		validate_args
 		push_cache
 	;;
-	fetch)
+	pull)
 		validate_args
-		fetch_cache
+		pull_cache
 	;;
 	--help|-h)
 		usage 0
