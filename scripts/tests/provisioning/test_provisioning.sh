5081704ce1f24958de3001342e14debf79fa77aa
Alex Stewart <alex.stewart@ni.com>
2021-04-22 15:03:28 -0500

    tests/provisioning: use the whole test output
    It's easier to use to use the entire stdout from the start-vm script
    when parsing for test success, rather than messing around passing log
    paths to tcl expect.
    
    Signed-off-by: Alex Stewart <alex.stewart@ni.com>

--- scripts/tests/provisioning/test_provisioning.sh
+++ scripts/tests/provisioning/test_provisioning.sh
@@ -9,7 +9,29 @@
 #  * evaluates the ptest results.
 
 script_dir="$(dirname "$0")"
-source "$script_dir/start-vm-using-expect.sh"
+
+usage () {
+	cat <<EOF
+$(basename $0) nilrt_vm_directory ipk_feeds [ipk_feeds...]
+EOF
+	exit ${1:-2}
+}
+
+if [ $# -lt 2 ]; then
+	echo "ERROR: invalid or missing arguments." >&2
+	usage
+fi
+
+vm_dir=${1}
+ipk_feeds=(${@:2})
+log_file=./$(basename ${BASH_SOURCE%.*}).log
+
+(
+	bash "$script_dir/start-vm-using-expect.sh" \
+		${ipk_feeds[@]/#/--ipk-feed /} \
+		"${vm_dir}" \
+		test_rauc_provision.expect
+) | tee $log_file
 
 # Quickly parse the test session log for pass/fail status
 # GREP will return code...
