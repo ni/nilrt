9dbd955a05c15746e1c2322f0502c0e4629f9bf8
Alex Stewart <alex.stewart@ni.com>
2022-01-05 15:59:43 -0600

    pipelines: update feed recipe names
    The "core" and "extra" package feeds in meta-nilrt have been renamed
    from packagegroup-ni-${feed} to packagefeed-ni-${feed}. Update their
    names in the pipeline scripts.
    
    Signed-off-by: Alex Stewart <alex.stewart@ni.com>

--- scripts/pipelines/build.core-feeds.sh
+++ scripts/pipelines/build.core-feeds.sh
@@ -5,5 +5,5 @@ SCRIPT_ROOT=$(realpath $(dirname $BASH_SOURCE))
 . "${SCRIPT_ROOT}/build.common.sh"
 
 echo "INFO: Building the core package feed."
-bitbake packagegroup-ni-coreimagerepo
+bitbake packagefeed-ni-core
 bitbake package-index
