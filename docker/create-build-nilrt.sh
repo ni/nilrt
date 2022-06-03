7fb47e66642e6e7718c0aba0c6059211bf8b2f5c
Alex Stewart <alex.stewart@ni.com>
2022-06-03 14:04:53 -0500

    create-build-nilrt.sh: tag image with codename
    The create-build-nilrt.sh script tags the resulting `build-nilrt`
    image as 'latest', in all cases. This is dangerous on systems which also
    build the sumo-mainline, as the user may inadvertently source into the
    wrong container image.
    
    Instead, source the NILRT codename from the meta-nilrt layer, and tag
    the image with the codename.
    
    Signed-off-by: Alex Stewart <alex.stewart@ni.com>
    (cherry picked from commit 19e02d6f28ea3cf1b6b594c5f27484ea46f446b7)
    Signed-off-by: Mike Petersen <mike.petersen@ni.com>

--- docker/create-build-nilrt.sh
+++ docker/create-build-nilrt.sh
@@ -45,12 +45,21 @@ pushd "${SCRIPT_ROOT}"
 short_hash=$(git rev-parse --short HEAD)
 popd
 
+# parse the NIRLT codename from the meta-nilrt:layer.conf
+NILRT_codename=$(grep -e '^LAYERSERIES_COMPAT_meta-nilrt' "${SCRIPT_ROOT}/../sources/meta-nilrt/conf/layer.conf" | cut -d'"' -f2)
+if [ -z "$NILRT_codename" ]; then
+	echo "ERROR: could not parse NILRT_codename from the meta-nilrt layer." >&2
+	exit 1
+else
+	echo "INFO: using NILRT_codename=${NILRT_codename}"
+fi
+
 
 set -e
 # build the pyrex base image
 docker build \
 	-f "${PYREX_ROOT}/image/Dockerfile" \
-	-t "pyrex-base:sumo" \
+	-t "pyrex-base:${NILRT_codename}" \
 	--build-arg=PYREX_BASE=$PYREX_BASE \
 	"${PYREX_ROOT}/image"
 
@@ -58,13 +67,13 @@ docker build \
 docker build \
 	-f "${SCRIPT_ROOT}/build-nilrt.Dockerfile" \
 	-t "${IMAGE_NAME}:${short_hash}" \
-	--build-arg=PYREX_IMAGE=pyrex-base:sumo \
+	--build-arg=PYREX_IMAGE=pyrex-base:${NILRT_codename} \
 	"${SCRIPT_ROOT}"
 
-# tag the image as 'latest'
+# tag the image with the NILRT codename
 docker tag \
 	"${IMAGE_NAME}:${short_hash}" \
-	"${IMAGE_NAME}:latest"
+	"${IMAGE_NAME}:${NILRT_codename}"
 
 # optionally tag it with the branch name
 if [ -n "${tag_branch}" ]; then
