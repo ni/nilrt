26fe40e596c6e198500d5659110fa231f3c6b94a
Can Wong <can.wong@ni.com>
2024-08-20 11:24:47 -0500

    build.core-feeds.sh: Update to build Kernel
    Signed-off-by: Can Wong <can.wong@ni.com>

--- scripts/pipelines/build.core-feeds.sh
+++ scripts/pipelines/build.core-feeds.sh
@@ -7,3 +7,6 @@ SCRIPT_ROOT=$(realpath $(dirname $BASH_SOURCE))
 echo "INFO: Building the core package feed."
 bitbake packagefeed-ni-core
 bitbake package-index
+
+# AB#2791248: Build the Linux Kernel to ensure CVEs are made available
+bitbake linux-nilrt
