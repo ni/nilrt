919a4ad4bcd4e0eb7c73969c9d9811729caa8675
Can Wong <can.wong@ni.com>
2024-09-09 08:35:55 -0500

    Revert "build.cross-toolchain.sh: Set SDK_ARCHIVE_TYPE to "zip""
    This reverts commit 7ea0563d37b89d6a03cd60e6ad740d3a5b28dfa3.
    
    Signed-off-by: Can Wong <can.wong@ni.com>
    (cherry picked from commit ad6bdac1eb1274d7dddb97c4dfad391f6ddf2d47)
    [cvadrevu: Reverts c342ccbd055dc6b17e7612877a075bc50c067d8a in this branch]
    Signed-off-by: Chaitanya Vadrevu <chaitanya.vadrevu@ni.com>

--- scripts/pipelines/build.cross-toolchain.sh
+++ scripts/pipelines/build.cross-toolchain.sh
@@ -6,7 +6,6 @@ SCRIPT_ROOT=$(realpath $(dirname $BASH_SOURCE))
 
 echo "INFO: Setting SDKMACHINE to x86_64-mingw32"
 export SDKMACHINE="x86_64-mingw32"
-export SDK_ARCHIVE_TYPE="zip"
 
 echo "INFO: Building meta-toolchain"
 bitbake meta-toolchain
