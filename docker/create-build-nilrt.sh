46a53461fc215bca3592f274b3a9168ca69c18a0
Chaitanya Vadrevu <chaitanya.vadrevu@emerson.com>
2025-03-31 16:58:09 -0500

    create-build-nilrt: Set PYREX_BASE to ubuntu 24.04
    Set PYREX_BASE to ubuntu-24.04-oe to be able to build 6.12 kernel.
    
    Also update build-nilrt.Dockerfile to use libtinfo6 instead of
    libtinfo5.
    
    Signed-off-by: Chaitanya Vadrevu <chaitanya.vadrevu@emerson.com>
    (cherry picked from commit 1fca3b6b865e058e1c04dd0f6f540eb2c5b82614)
    Signed-off-by: Chaitanya Vadrevu <chaitanya.vadrevu@emerson.com>

--- docker/create-build-nilrt.sh
+++ docker/create-build-nilrt.sh
@@ -3,7 +3,7 @@ set -u
 
 SCRIPT_ROOT=$(realpath $(dirname ${BASH_SOURCE}))
 PYREX_ROOT=$(realpath "${SCRIPT_ROOT}/../sources/pyrex")
-PYREX_BASE=ubuntu-22.04-oe
+PYREX_BASE=ubuntu-24.04-oe
 
 IMAGE_NAME=build-nilrt
 
